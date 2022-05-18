f80835cd19aeb8ebff92f4d5c263c6547a0f4e75
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-05-18 17:59:26 -0400

    attempted to resolve issue with generics

--- project-onboarding/wk1/src/com/revature/stack_min/CustomStack.java
+++ project-onboarding/wk1/src/com/revature/stack_min/CustomStack.java
@@ -2,12 +2,15 @@ package com.revature.stack_min;
 
 import java.util.Stack;
 
-public class CustomStack {
+public class CustomStack<E> {
   // properties: stack of elements and minElement -- can the min el be generic?
   Stack<E> mainStack;
   Stack<E> minElStack;
 
-
+  public CustomStack(Stack<E> mainStack, Stack<E> minElStack) {
+    this.mainStack = mainStack;
+    this.minElStack = minElStack;
+  }
 
   void push(E element) {
     mainStack.push(element);
@@ -21,7 +24,8 @@ public class CustomStack {
     // check against min element -- what happens if min element == pop value?
   }
 
-  void min() {
-  // return min value
+  E min() {
+    //return min value
+    return E;
   }
 }
