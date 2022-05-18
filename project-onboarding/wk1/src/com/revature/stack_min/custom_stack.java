841bb0c13e37643ea1e78898009266f6b983cbf6
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-05-18 14:47:38 -0400

    addtional pseudocode, attempting to create generic stack

--- project-onboarding/wk1/src/com/revature/stack_min/custom_stack.java
+++ project-onboarding/wk1/src/com/revature/stack_min/custom_stack.java
@@ -1,21 +1,22 @@
 package com.revature.stack_min;
 
-public class Stack {
-  // properties: stack of elements and minElement -- can the min el be generic?
-  stack<E> myStack;
-  E minElement;
+import java.util.Stack;
 
-  push() {
+public class custom_stack {
+  // properties: stack of elements and minElement -- can the min el be generic?
+  Stack<E> myStack;
+
+  void push(int element) {
     // add to tail/top of stack
     // check min element against value pushed to stack?
   }
 
-  pop() {
+  void pop() {
     // remove from tail/top of stack
     // check against min element -- what happens if min element == pop value?
   }
 
-  min() {
+   min() {
   // return min value
   }
 }
